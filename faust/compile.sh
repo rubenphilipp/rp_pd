a35cc78da58ca4e719f15c918222ce2f58d9a719
Ruben Philipp <me@rubenphilipp.com>
2025-06-22 01:00:57 +0200

    ambi externals

--- faust/compile.sh
+++ faust/compile.sh
@@ -4,9 +4,10 @@
 ##
 ## Author: Ruben Philipp <me@rubenphilipp.com>
 ## Created: 2025-04-14
-## $$ Last modified:  16:15:11 Mon Apr 14 2025 CEST
+## $$ Last modified:  15:29:03 Sat Jun 21 2025 CEST
 ################################################################################
 
+## Limiter
 faust2puredata limiter.dsp
 
 ################################################################################
